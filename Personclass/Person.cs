86dc996328f64d6c191fe21c7358d852e2391fad
AliyevMura <7lvghl9@code.edu.az>
2023-04-13 11:51:55 +0400

    initial commit

---
+++ Personclass/Person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task13april.Personclass;
+using Task13april;
+namespace Task13april.Personclass
+{
+    internal class Person
+    {
+
+        private string _username;
+        private string _password;
+        private string _name;
+        private string _surname;
+        private int _age;
+        private int _birthday;
+
+
+
+
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+            }
+        }
+
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+               _password = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+               _surname = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+               
+        }
+        public int Birthday
+        {
+            get
+            {
+                return _birthday;
+            }
+            set
+            {
+               _birthday = value;
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Task13april.Personclass;
+namespace Task13april
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "M";
+            person.Surname = "Aliyev";
+            person.Age = 19;
+            person.Birthday = 1969;
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Surname);
+            Console.WriteLine(person.Birthday);
+        }
+    }
+}
